1aa55df7dad70f67742b8e9800414137aa533fbb
sebastianselander <sebastian.selander@gmail.com>
2023-11-10 18:53:21 +0100

    main

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+import           Control.Monad
+import           Data.Time.Calendar
+import           Data.Time.Clock
+import           System.Environment
+
+mains :: [String -> IO ()]
+mains = undefined
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    -- cabal run aoc23 -- <FILE>
+    args <- getArgs
+    (_, _, currentDay) <-  toGregorian . utctDay <$> getCurrentTime
+    case args of
+        [input]      -> mains !! currentDay $ input
+        [input, day] -> (mains !! read day) input
+        xs            -> error $ "Incorrect amount of arguments: " ++ show (length xs)
