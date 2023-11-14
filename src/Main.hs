004b2fcef693b484830091eb3f5adc33a31fb536
sebastianselander <sebastian.selander@gmail.com>
2023-11-14 19:08:09 +0100

    Pilling for no reason

--- src/Main.hs
+++ src/Main.hs
@@ -3,22 +3,16 @@
 
 module Main where
 
-import           Data.Time.Calendar
-import           Data.Time.Clock
-import           System.Directory
-import           System.Environment
-import           System.Exit
-import           System.IO
-import           Text.Read
+import Data.Time.Calendar
+import Data.Time.Clock
+import System.Directory
+import System.Environment
+import System.Exit
+import System.IO
+import Text.Read
 
-mains :: [String -> IO ()]
-mains =
-    let
-        impls = []
-    in
-        impls ++ replicate
-                    (25 - length impls)
-                    (putStrLn . const "Not yet implemented")
+mains :: [(Int, String -> IO ())]
+mains = []
 
 inputFilePrefix :: FilePath
 inputFilePrefix = "day"
@@ -26,47 +20,62 @@ inputFilePrefix = "day"
 inputDir :: FilePath
 inputDir = "inputs/"
 
+makeISO :: Year -> MonthOfYear -> DayOfMonth -> String
+makeISO year month day = concat [show year, "-", show month, "-", show day]
+
 main :: IO ()
 main = do
     args <- getArgs
-    (_, _, currentDay) <-  toGregorian . utctDay <$> getCurrentTime
+    (year, month, day) <- toGregorian . utctDay <$> getCurrentTime
+    putStrLn $ "Today is: " ++ makeISO year month day
     case args of
-        [] -> parseFile (inputDir ++ inputFilePrefix ++ show currentDay) >>= execute currentDay mains
-        [input] -> parseFile input >>= execute currentDay mains
-        [input, day] -> do
-            input <- parseFile input
-            dayNumber <- case parseDay day of
-                Nothing -> errExit $ unwords [
-                    "Could not parse day",
-                    quote day,
-                    "as an integer in the range [1 .. 25]"]
-                Just n  -> pure n
-            execute dayNumber mains input
-        xs -> errExit $ concat
-            [ "Incorrect amount of arguments "
-            , quote $ show (length xs)
-            , "\n\n"
-            , "The program is run by the following command: 'cabal run aoc23 -- <FILE> <DAY>'"
-            , "\n"
-            , "<DAY> is optional, if left blank it runs the current day"]
+        [] -> do
+            f <- execute day mains
+            file <- parseFile (inputDir ++ inputFilePrefix ++ show day)
+            f file
+        [inputDay] -> do
+            dayNumber <- case parseDay inputDay of
+                Nothing ->
+                    errExit
+                        $ unwords
+                            [ "Could not parse day"
+                            , quote inputDay
+                            , "as an integer in the range [1 .. 25]"
+                            ]
+                Just n -> pure n
+            f <- execute dayNumber mains
+            input <- parseFile (inputDir ++ inputFilePrefix ++ inputDay)
+            f input
+        xs ->
+            errExit
+                $ concat
+                    [ "Incorrect amount of arguments "
+                    , quote $ show (length xs)
+                    , "\n\n"
+                    , "The program is run by the following command: 'cabal run aoc23 -- <FILE> <DAY>'"
+                    , "\n"
+                    , "<DAY> is optional, if left blank it runs the current day"
+                    ]
 
-execute :: Int -> [String -> IO ()] -> String -> IO ()
-execute n fs input
-  | length fs >= n = (fs !! (n - 1)) input
-  | otherwise         = errExit $ "Program for day " ++ quote (show n) ++ " does not exist"
+execute :: Int -> [(Int, String -> IO ())] -> IO (String -> IO ())
+execute n fs = case lookup n fs of
+    Just f -> pure f
+    Nothing -> errExit $ "Program for day " ++ quote (show n) ++ " does not exist"
 
 parseFile :: String -> IO String
-parseFile inputFile
-    = doesFileExist inputFile >>= \case
-        False -> hPutStrLn stderr ("The file " ++ quote inputFile ++ " does not exist")
-                 >> exitFailure
+parseFile inputFile =
+    doesFileExist inputFile >>= \case
+        False ->
+            hPutStrLn stderr ("The file " ++ quote inputFile ++ " does not exist")
+                >> exitFailure
         True -> readFile inputFile
 
 parseDay :: String -> Maybe Int
 parseDay day
-  | Just n <- readMaybe @Int day
-  , n >= 1 && n <= 25 = Just n
-  | otherwise = Nothing
+    | Just n <- readMaybe @Int day
+    , n >= 1 && n <= 25 =
+        Just n
+    | otherwise = Nothing
 
 errExit :: String -> IO a
 errExit str = hPutStrLn stderr str >> exitFailure
