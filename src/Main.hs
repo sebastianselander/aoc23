845d206e16430119ae2ba4bdfe29782f46dee258
sebastianselander <sebastian.selander@gmail.com>
2023-11-28 12:24:14 +0100

    format

--- src/Main.hs
+++ src/Main.hs
@@ -9,7 +9,11 @@ import System.Exit
 import System.IO
 import Text.Read
 
-data AOC = Aoc { day :: Int, part1 :: String -> String, part2 :: String -> String}
+data AOC = Aoc
+    { day :: Int
+    , part1 :: String -> String
+    , part2 :: String -> String
+    }
 
 mains :: [AOC]
 mains = []
@@ -28,19 +32,19 @@ clean = setCursorPosition 0 0 >> clearScreen
 
 prettyDate :: Year -> MonthOfYear -> DayOfMonth -> IO ()
 prettyDate y m d = do
-  let today = "Today's date: "
-  let date = makeISO y m d
-  putStr today
-  setSGR [SetItalicized True]
-  putStrLn date
-  setSGR [Reset]
-  setSGR
-    [ SetConsoleIntensity BoldIntensity,
-      SetColor Foreground Dull Green
-    ]
-  putStrLn $ titleBar (length today + length date)
-  setSGR [Reset]
-  hFlush stdout
+    let today = "Today's date: "
+    let date = makeISO y m d
+    putStr today
+    setSGR [SetItalicized True]
+    putStrLn date
+    setSGR [Reset]
+    setSGR
+        [ SetConsoleIntensity BoldIntensity
+        , SetColor Foreground Dull Green
+        ]
+    putStrLn $ titleBar (length today + length date)
+    setSGR [Reset]
+    hFlush stdout
 
 titleBar :: Int -> String
 titleBar n = replicate n '='
@@ -48,49 +52,48 @@ titleBar n = replicate n '='
 data Part = Part1 | Part2
 
 instance Show Part where
-  show Part1 = "1"
-  show Part2 = "2"
+    show Part1 = "1"
+    show Part2 = "2"
 
 main :: IO ()
 main = do
-  clean
-  args <- getArgs
-  (year, month, day) <- toGregorian . utctDay <$> getCurrentTime
-  prettyDate year month day
-  case args of
-    [inputDay] -> do
-      dayNumber <- parseDay inputDay
-      f1 <- execute dayNumber Part1 mains
-      f2 <- execute dayNumber Part2 mains
-      input <- parseFile (inputDir ++ inputFilePrefix ++ inputDay)
-      putStrLn $ "Part1: " ++ f1 input
-      putStrLn $ "Part2: " ++ f2 input
-    [inputDay, inputPart] -> do
-      dayNumber <- parseDay inputDay
-      partNumber <- parsePart inputPart
-      input <- parseFile (inputDir ++ inputFilePrefix ++ inputDay)
-      f <- execute dayNumber partNumber mains
-      putStrLn $ "Part" ++ show partNumber ++ ": " ++ f input
-    [inputDay, inputPart, inputFile] -> do
-      dayNumber <- parseDay inputDay
-      partNumber <- parsePart inputPart
-      input <- parseFile inputFile
-      f <- execute dayNumber partNumber mains
-      putStrLn $ "Part" ++ show partNumber ++ ": " ++ f input
-    xs ->
-      errExit $
-        concat
-          [ "Incorrect amount of arguments: ",
-            quote $ show (length xs),
-            "\n\n",
-            "The program is run by one of the following commands:\n",
-            quote "cabal run aoc23 -- <DAY> <PART> <INPUTFILE>",
-            "\nor\n",
-            quote "cabal run aoc23 -- <DAY> <PART>",
-            "\nor\n",
-            quote "cabal run aoc23 -- <DAY>"
-          ]
-
+    clean
+    args <- getArgs
+    (year, month, day) <- toGregorian . utctDay <$> getCurrentTime
+    prettyDate year month day
+    case args of
+        [inputDay] -> do
+            dayNumber <- parseDay inputDay
+            f1 <- execute dayNumber Part1 mains
+            f2 <- execute dayNumber Part2 mains
+            input <- parseFile (inputDir ++ inputFilePrefix ++ inputDay)
+            putStrLn $ "Part1: " ++ f1 input
+            putStrLn $ "Part2: " ++ f2 input
+        [inputDay, inputPart] -> do
+            dayNumber <- parseDay inputDay
+            partNumber <- parsePart inputPart
+            input <- parseFile (inputDir ++ inputFilePrefix ++ inputDay)
+            f <- execute dayNumber partNumber mains
+            putStrLn $ "Part" ++ show partNumber ++ ": " ++ f input
+        [inputDay, inputPart, inputFile] -> do
+            dayNumber <- parseDay inputDay
+            partNumber <- parsePart inputPart
+            input <- parseFile inputFile
+            f <- execute dayNumber partNumber mains
+            putStrLn $ "Part" ++ show partNumber ++ ": " ++ f input
+        xs ->
+            errExit $
+                concat
+                    [ "Incorrect amount of arguments: "
+                    , quote $ show (length xs)
+                    , "\n\n"
+                    , "The program is run by one of the following commands:\n"
+                    , quote "cabal run aoc23 -- <DAY> <PART> <INPUTFILE>"
+                    , "\nor\n"
+                    , quote "cabal run aoc23 -- <DAY> <PART>"
+                    , "\nor\n"
+                    , quote "cabal run aoc23 -- <DAY>"
+                    ]
 
 execute :: Int -> Part -> [AOC] -> IO (String -> String)
 execute n part xs = do
@@ -100,43 +103,46 @@ execute n part xs = do
         Part2 -> pure $ part2 aoc
   where
     find :: Int -> [AOC] -> IO AOC
-    find _ [] = errExit $ "Program for day " ++ quote (show n) ++ " does not exist"
-    find m (y:ys)
-      | m == day y = pure y
-      | otherwise = find m ys
+    find _ [] =
+        errExit $ "Program for day " ++ quote (show n) ++ " does not exist"
+    find m (y : ys)
+        | m == day y = pure y
+        | otherwise = find m ys
 
 parseFile :: String -> IO String
 parseFile inputFile =
-  doesFileExist inputFile >>= \case
-    False ->
-      hPutStrLn stderr ("The file " ++ quote inputFile ++ " does not exist")
-        >> exitFailure
-    True -> readFile inputFile
+    doesFileExist inputFile >>= \case
+        False ->
+            hPutStrLn
+                stderr
+                ("The file " ++ quote inputFile ++ " does not exist")
+                >> exitFailure
+        True -> readFile inputFile
 
 parseDay :: String -> IO Int
 parseDay day
-  | Just n <- readMaybe @Int day,
-    n >= 1 && n <= 25 =
-      pure n
-  | otherwise =
-      errExit $
-        unwords
-          [ "Could not parse day",
-            quote day,
-            "as an integer in the range [1 .. 25]"
-          ]
+    | Just n <- readMaybe @Int day
+    , n >= 1 && n <= 25 =
+        pure n
+    | otherwise =
+        errExit $
+            unwords
+                [ "Could not parse day"
+                , quote day
+                , "as an integer in the range [1 .. 25]"
+                ]
 
 parsePart :: String -> IO Part
 parsePart part
-  | Just 1 <- readMaybe @Int part = pure Part1
-  | Just 2 <- readMaybe @Int part = pure Part2
-  | otherwise = do
-      errExit $
-        unwords
-          [ "Could not parse part",
-            quote part,
-            "as an integer in the range [1 .. 2]"
-          ]
+    | Just 1 <- readMaybe @Int part = pure Part1
+    | Just 2 <- readMaybe @Int part = pure Part2
+    | otherwise = do
+        errExit $
+            unwords
+                [ "Could not parse part"
+                , quote part
+                , "as an integer in the range [1 .. 2]"
+                ]
 
 errExit :: String -> IO a
 errExit str = hPutStrLn stderr str >> exitFailure
