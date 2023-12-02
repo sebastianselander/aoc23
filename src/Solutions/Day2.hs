b2ef475d5996d2c721b3ed7d66530affd207d5fa
sebastianselander <sebastian.selander@gmail.com>
2023-12-02 11:34:00 +0100

    Golfed hard

--- src/Solutions/Day2.hs
+++ src/Solutions/Day2.hs
@@ -6,58 +6,36 @@ import Text.Megaparsec.Char qualified as P
 import Text.Megaparsec.Char.Lexer qualified as L
 
 data Color = Red | Green | Blue
-    deriving (Show)
+    deriving (Show, Enum)
 
-data Game = Game Int [[(Int, Color)]]
-    deriving (Show)
-
-pColor :: Parser (Int, Color)
-pColor = do
-    n <- L.decimal <* P.space
-    color <-
-        "blue"
-            $> Blue
-            <|> "red"
-            $> Red
-            <|> "green"
-            $> Green
-    pure (n, color)
-
-pOnePick :: Parser [(Int, Color)]
-pOnePick = pColor `P.sepBy` ", "
-
-pGame :: Parser Game
-pGame = do
-    ident <- "Game " *> L.decimal <* ": "
-    res <- pOnePick `P.sepBy` "; "
-    pure $ Game ident res
+data Game = Game { ident :: Int , games :: [[(Int, Color)]]
+    } deriving (Show)
 
 parse :: Parser [Game]
-parse = P.some (pGame <* "\n") <* P.eof
-
-impossible :: Game -> Bool
-impossible (Game _ games) = any (any f) games
-  where
-    f :: (Int, Color) -> Bool
-    f (n, Red) = n > 12
-    f (n, Green) = n > 13
-    f (n, Blue) = n > 14
+parse = P.some (Game
+            <$> ("Game " *> L.decimal <* ": ")
+            <*> ((,) <$> (L.decimal <* P.space)
+                            <*> ("blue" $> Blue
+                                    <|> "red" $> Red
+                                    <|> "green" $> Green))
+            `P.sepBy` ", " `P.sepBy` "; " <* "\n") <* P.eof
 
 p1 :: Text -> Int
 p1 = sum
    . map (\(Game n _) -> n)
-   . filter (not . impossible)
+   . filter (not . any (any (\(n, c) -> n > 12 + fromEnum c)) . games)
    . fromJust
    . P.parseMaybe parse
 
-maxes :: Game -> Int
-maxes (Game _ xs) = maximum [ r * g * b
-                            | (r, Red) <- concat xs
-                            , (g, Blue) <- concat xs
-                            , (b, Green) <- concat xs
-                            ]
 p2 :: Text -> Int
-p2 = sum . map maxes . fromJust . P.parseMaybe parse
+p2 = sum
+   . map (\xs ->
+            maximum [ r * g * b
+                    | (r, Red) <- concat xs.games
+                    , (g, Blue) <- concat xs.games
+                    , (b, Green) <- concat xs.games])
+   . fromJust
+   . P.parseMaybe parse
 
 solve :: AOC
 solve = AOC 2 p1 p2
