e123ec59915aef95f0fa1bc220da55928b3d7886
sebastianselander <sebastian.selander@gmail.com>
2023-12-07 09:16:51 +0100

    remove 1 lul

--- src/Solutions/Day07.hs
+++ src/Solutions/Day07.hs
@@ -5,23 +5,27 @@ import Data.Text qualified as Text
 import Lude
 
 normal, joker :: String
-normal = "123456789TJQKA"
-joker = "J123456789TQKA"
+normal = "23456789TJQKA"
+joker = "J23456789TQKA"
 
 parse :: Text -> [(String, Int)]
 parse = map (second read . listToTuple . words . Text.unpack) . Text.lines
 
 order :: String -> Int
 order = go . sort . map length . group . sort
-  where
-    go [1, 1, 1, 1, 1] = 0
-    go [1, 1, 1, 2]    = 1
-    go [1, 2, 2]       = 2
-    go [1, 1, 3]       = 3
-    go [2, 3]          = 4
-    go [1, 4]          = 5
-    go [5]             = 6
-    go _ = error "Not a hand"
+  where go = \case
+          [1, 1, 1, 1, 1] -> 0
+          [1, 1, 1, 2]    -> 1
+          [1, 2, 2]       -> 2
+          [1, 1, 3]       -> 3
+          [2, 3]          -> 4
+          [1, 4]          -> 5
+          [5]             -> 6
+          _               -> error "not a hand"
+
+jokerOrder :: String -> Int
+jokerOrder str =
+    maximum $ map (order . \x -> replace "J" (singleton x) str) joker
 
 compareHands :: String -> (String -> Int) -> String -> String -> Ordering
 compareHands cr f as bs = case comparing f as bs of
@@ -42,10 +46,6 @@ p1 = sum
    . map snd
    . sortBy (compareHands normal order `on` fst)
    . parse
-
-jokerOrder :: String -> Int
-jokerOrder str =
-    maximum $ map (order . \x -> replace "J" (singleton x) str) joker
 
 p2 :: Text -> Int
 p2 = sum
