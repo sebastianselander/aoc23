48d678c36d4a7ac0b9f3c0ea0b7c6195f7a92690
sebastianselander <sebastian.selander@gmail.com>
2023-12-09 13:20:52 +0100

    format

--- src/Day09.hs
+++ src/Day09.hs
@@ -5,8 +5,9 @@ import Data.Text qualified as Text (lines, unpack)
 import Lude
 
 step :: ([Int] -> Int) -> [Int] -> [Int]
-step f xs | allSame xs = [f xs]
-          | otherwise = f xs : step f (zipWith (-) (tail xs) xs)
+step f xs
+    | allSame xs = [f xs]
+    | otherwise = f xs : step f (zipWith (-) (tail xs) xs)
 
 sol :: ([Int] -> Int) -> ([Int] -> Int) -> Text -> Int
 sol f g = sum . map ((f . step g) . map read . words . Text.unpack) . Text.lines
