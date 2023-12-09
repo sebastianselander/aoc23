72120f32a7309fc0a920b380be8bab247f077ba8
sebastianselander <sebastian.selander@gmail.com>
2023-12-09 09:47:06 +0100

    shorter

--- src/Day09.hs
+++ src/Day09.hs
@@ -4,27 +4,19 @@ import Data.List.Extra
 import Data.Text qualified as Text
 import Lude
 
-parse :: Text -> [[Int]]
-parse = map (map read . words . Text.unpack) . Text.lines
-
-diff :: [Int] -> [Int]
-diff xs = zipWith (-) (tail xs) xs
-
-next :: [Int] -> Int
-next xs
-  | allSame xs = last xs
-  | otherwise = last xs + next (diff xs)
-
-prev :: [Int] -> [Int]
-prev xs
-  | allSame xs = [head xs]
-  | otherwise = head xs : prev (diff xs)
+step :: ([Int] -> Int) -> [Int] -> [Int]
+step f xs
+    | allSame xs = [f xs]
+    | otherwise = f xs : step f (zipWith (-) (tail xs) xs)
 
 p1 :: Text -> Int
-p1 = sum . map next . parse
+p1 = sum . map ((sum . step last) . map read . words . Text.unpack) . Text.lines
 
 p2 :: Text -> Int
-p2 = sum . map (foldr (-) 0 . prev) . parse
+p2 =
+    sum
+        . map ((foldr (-) 0 . step head) . map read . words . Text.unpack)
+        . Text.lines
 
 solve :: AOC
 solve = AOC 9 p1 p2
