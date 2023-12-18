e1fb02ba1429fb8dbf81e583aaf88be18c62a10d
sebastianselander <sebastian.selander@gmail.com>
2023-12-18 17:06:32 +0100

    day 18, didnt know equation, I guess I learned something

--- src/Day18.hs
+++ src/Day18.hs
@@ -1,38 +1,56 @@
 module Day18 (solve) where
 
+import Data.Bifunctor (bimap)
 import Data.List.Extra (dropEnd)
-import Data.Set (Set)
-import Data.Set qualified as Set
-import Data.Text (pack, unpack)
+import Data.Text (unpack)
 import Lude
-import Control.Monad.State
+import Numeric
 
 parse :: Text -> [(Char, Int, String)]
 parse = map (clean . words) . lines . unpack
   where
-    clean [a, b, c] = (head a, read b, dropEnd 1 $ tail c)
+    clean [a, b, c] = (head a, read b, drop 2 $ dropEnd 1 c)
 
-border :: (Int, Int) -> [(Char, Int, String)] -> Set (Int, Int)
-border _ [] = Set.empty
-border (row, col) ((char, step, _) : xs) =
-    let (ix', ixs) = case char of
-            'U' -> ((row - step, col), [(x, col) | x <- [row - 1, row - 2 .. row - step]])
-            'D' -> ((row + step, col), [(x, col) | x <- [row + 1 .. row + step]])
-            'L' -> ((row, col - step), [(row, y) | y <- [col - 1, col - 2 .. col - step]])
-            'R' -> ((row, col + step), [(row, y) | y <- [col + 1 .. col + step]])
-            _ -> error "unreachable!"
-     in set ixs `Set.union` border ix' xs
+border :: Bool -> [(Char, Int, String)] -> (Int, [(Int, Int)])
+border b xs = go b (0, 0) xs
+  where
+    go _ _ [] = (0, [])
+    go b (row, col) ((char', step', hex) : xs) =
+        let step =
+                if b
+                    then fst $ head $ readHex (init hex)
+                    else step'
+            char =
+                if b
+                    then digToChar $ last hex
+                    else char'
+         in let ix = case char of
+                    'U' -> (row - step, col)
+                    'D' -> (row + step, col)
+                    'L' -> (row, col - step)
+                    'R' -> (row, col + step)
+                    _ -> error "unreachable!"
+             in bimap (+ step) (ix :) (go b ix xs)
+      where
+        digToChar '0' = 'R'
+        digToChar '1' = 'D'
+        digToChar '2' = 'L'
+        digToChar '3' = 'U'
+        digToChar _ = error "invalid char"
 
-flood :: (Int, Int) -> State (Set (Int, Int)) ()
-flood (row, col)
-   = do
-      visited <- get
-      put (Set.insert (row, col) visited)
-      let ns = filter (`Set.notMember` visited) [(row, col + 1), (row, col - 1), (row + 1, col), (row - 1, col)]
-      mapM_ flood  ns
+area :: (Int, [(Int, Int)]) -> Int
+area (perim, xs) = (abs (go xs) + perim) `div` 2
+  where
+    (xl, yl) = head xs
+    go [] = 0
+    go [(xn, yn)] = xn * yl - yn * xl
+    go ((x1, y1) : (x2, y2) : xs) = (x1 * y2 - y1 * x2) + go ((x2, y2) : xs)
 
 p1 :: Text -> Int
-p1 = Set.size . execState (flood (1,1)) . border (0, 0) . parse
+p1 = succ . area . border False . parse
+
+p2 :: Text -> Int
+p2 = succ . area . border True . parse
 
 solve :: AOC
-solve = AOC 18 p1 id
+solve = AOC 18 p1 p2
